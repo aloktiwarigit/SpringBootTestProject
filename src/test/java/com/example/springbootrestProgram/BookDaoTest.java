41936f55885da26c96876ad49759ca163c32984d
Alok Tiwari <alokkumar.tiwari@cognizant.com>
2019-08-10 10:37:56 +0530

    first commit

--- src/test/java/com/example/springbootrestProgram/BookDaoTest.java
+++ src/test/java/com/example/springbootrestProgram/BookDaoTest.java
@@ -7,9 +7,9 @@ import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.springbootrestProgram.dao.BooksDao;
@@ -18,7 +18,7 @@ import com.example.springbootrestProgram.entity.Books;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration
 public class BookDaoTest {
 	
 	@Autowired
--- src/test/java/com/example/springbootrestProgram/BookServiceTest.java
+++ src/test/java/com/example/springbootrestProgram/BookServiceTest.java
@@ -19,9 +19,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.springbootrestProgram.dao.BooksDao;
@@ -30,7 +30,7 @@ import com.example.springbootrestProgram.service.BooksServiceImpl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration
 public class BookServiceTest {
 
 
