e7c9ed14e757e616c83a986ed7e0db9894c611c7
Alok Tiwari <alokkumar.tiwari@cognizant.com>
2019-08-10 11:31:06 +0530

    first commit

--- src/test/java/com/example/springbootrestProgram/SpringbootrestProgramApplicationTests.java
+++ src/test/java/com/example/springbootrestProgram/SpringbootrestProgramApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -12,6 +13,7 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ComponentScan(basePackages = {"com.example"})
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = AppConfig.class, loader = AnnotationConfigContextLoader.class)
 public class SpringbootrestProgramApplicationTests {
